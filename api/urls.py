b757153df3d1534160ea8900f32ea4fe79c5d506
Apus <jhohannarose.apus@msunaawan.edu.ph>
2025-10-16 20:13:43 +0800

    Class-based student

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from api.views import function_base_view
+from  api.views import class_base_view
 
 urlpatterns = [
     path('fbv-students/', function_base_view.studentView),
     path('fbv-students/<int:student_id>/', function_base_view.student),
     path('fbv-employees/', function_base_view.employeeView),
-    path('fbv-employees/<int:emp_id>/', function_base_view.employee)
+    path('fbv-employees/<int:emp_id>/', function_base_view.employee),
+
+    path('fbv-student/', class_base_view.StudentView.as_view())
 ]
